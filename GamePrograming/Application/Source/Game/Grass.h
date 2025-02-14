f57222839c5bd45c9054d99e6b57af479b74451c
soraemonzaemon <soraemon21@yahoo.co.jp>
2025-02-14 12:18:47 +0900

    ゲームオブジェクトの5つ追加

---
+++ GamePrograming/Application/Source/Game/Grass.h
@@ -0,0 +1,21 @@
+/******************************************************
+* Grass.h	草
+* 制作者：センバソラ
+* 作成日：2024/12/05
+* 最終更新日：2025/2/13
+*******************************************************/
+#pragma once
+
+#include "Game/ThrowObject.h"
+
+class Grass : public ThrowObject {
+public:
+	Grass() = delete;
+	Grass(float x, float y, float r);
+	~Grass();
+
+private:
+
+};
+
+
